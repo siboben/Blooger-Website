60d5f7dc36af0b32ca51acd3a68b35664d039e20
siboben <sibomanabenjamin90@gmail.com>
2023-12-05 11:12:34 +0200

    first commit

---
+++ index.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Blooger</title>
+<!--------------fontsawesome------------------->
+<link rel="stylesheet" href="./css/all.css">
+    <!-----customer sytle-->
+    <link rel="stylesheet" href="./css/style.css">
+</head>
+<body>
+
+<!-----------------navigation----------------------------------------------->
+<nav class="nav">
+<div class="nav-menu flex-row">
+<div class=" nav-brand">
+<a href="#" class="text-gray">Blooger</a>
+</div>
+<div>
+<ul class="nav-items">
+<li class="nav-link">
+    <a href="#">Home</a>
+</li>
+<li class="nav-link">
+    <a href="#">Category</a>
+</li>
+<li class="nav-link">
+    <a href="#">Archives</a></li>
+    <li class="nav-link">
+    <a href="#">Pages</a>
+</li>
+    <li class="nav-link">
+    <a href="#">Contact us</a>
+</li>
+</ul>
+</div>
+<div class="social text-gray">
+
+<a href="#"><i class="fab fa-facebook-f"></i></a>
+<a href="#"><i class="fab fa-instagram"></i></a>
+<a href="#"><i class="fab fa-twitter"></i></a>
+<a href="#"><i class="fab fa-youtube"></i></a>
+</div>
+</div>
+</nav>
+<!-------------x-----------navigation-----------x------------------------------------------------->
+    <!--------customerjavascript------>
+  <script src="./js/main.js"></script>  
+</body>
+</html>
